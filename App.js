bdbbc6bda17d7e358e3248302e6ee810d69a28d8
Gilson Viana <vianawebdev@gmail.com>
2019-04-09 14:56:37 -0400

    :construction: React Navigation and screens added.

--- App.js
+++ App.js
@@ -1,37 +1,41 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { createStackNavigator, createAppContainer } from 'react-navigation'
+import HomeScreen from './src/screens/HomeScreen';
 
-import SearchBox from './src/components/SearchBox'
-import TrackList from './src/components/TrackList';
-
-export default class App extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      data: {}
-    }
-
-    this.handleSearchBoxSubmit = this.handleSearchBoxSubmit.bind(this)
-  }
-
-  handleSearchBoxSubmit(response) {
-    return this.setState({ data: response.tracks.items })    
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <SearchBox handleSearchBoxSubmit={this.handleSearchBoxSubmit} />
-        <TrackList list={this.state.data} />
-      </View>
-    )
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,        
-    marginTop: 32 
-  },
+const MainNavigator = createStackNavigator({
+  Home: {screen: HomeScreen}  
 })
+
+const App = createAppContainer(MainNavigator)
+
+export default App
+// export default class App extends React.Component {
+//   constructor(props) {
+//     super(props)
+
+//     this.state = {
+//       data: {}
+//     }
+
+//     this.handleSearchBoxSubmit = this.handleSearchBoxSubmit.bind(this)
+//   }
+
+//   handleSearchBoxSubmit(response) {
+//     return this.setState({ data: response.tracks.items })    
+//   }
+
+//   render() {
+//     return (
+//       <View style={styles.container}>
+        
+//       </View>
+//     )
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,        
+//     marginTop: 32 
+//   },
+// })
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { View, StyleSheet } from 'react-native'
+
+import SearchBox from '../components/SearchBox'
+import TrackList from '../components/TrackList'
+
+class HomeScreen extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            data: {}
+        }
+
+        this.handleSearchBoxSubmit = this.handleSearchBoxSubmit.bind(this)
+    }
+
+    handleSearchBoxSubmit(response) {
+        return this.setState({ data: response.tracks.items })
+    }
+
+    render() {
+        return(
+            <View style={styles.container}>
+                <SearchBox handleSearchBoxSubmit={this.handleSearchBoxSubmit} />
+                <TrackList list={this.state.data} />
+            </View>
+        )
+    }
+}
+    
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,        
+      marginTop: 32 
+    },
+  })
+
+export default HomeScreen
